7f68008f13dd3eef6b1b224bb7f3effa6f980916
Ivan Panchenko <arigatory@gmail.com>
2025-12-29 02:06:01 +0300

    iter1: initial task

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,101 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type Subj struct {
+	Product string `json:"name"`
+	Price   int    `json:"price"`
+}
+
+func JSONHandler(w http.ResponseWriter, req *http.Request) {
+	// собираем данные
+	subj := Subj{"Milk", 50}
+	// кодируем в JSON
+	resp, err := json.Marshal(subj)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	// устанавливаем заголовок Content-Type
+	// для передачи клиенту информации, кодированной в JSON
+	w.Header().Set("content-type", "application/json")
+	// устанавливаем код 200
+	w.WriteHeader(http.StatusOK)
+	// пишем тело ответа
+	w.Write(resp)
+}
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	body := fmt.Sprintf("Method: %s\r\n", req.Method)
+	body += "Header ===============\r\n"
+	for k, v := range req.Header {
+		body += fmt.Sprintf("%s: %v\r\n", k, v)
+	}
+	body += "Query parameters ===============\r\n"
+	for k, v := range req.URL.Query() {
+		body += fmt.Sprintf("%s: %v\r\n", k, v)
+	}
+	res.Write([]byte(body))
+}
+
+func apiPage(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("Это страница /api."))
+}
+
+func updateHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	path := strings.Trim(req.URL.Path, "/")
+	parts := strings.Split(path, "/")
+
+	log.Printf("Received request: %s, parts: %v", path, parts)
+
+	if len(parts) != 4 {
+		http.Error(res, "Bad request. Need 4 parts: update/{type}/{name}/{value}", http.StatusBadRequest)
+		return
+	}
+
+	metricType := parts[1]
+	metricName := parts[2]
+	metricValue := parts[3]
+
+	if metricType == "" {
+		http.Error(res, "Metric type is required", http.StatusBadRequest)
+		return
+	}
+
+	metricName = strings.TrimSpace(metricName)
+	if metricName == "" {
+		http.Error(res, "Metric name is required", http.StatusNotFound)
+		return
+	}
+
+	log.Printf("Type: %s, Name: %s, Value: %s", metricType, metricName, metricValue)
+
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/api/`, apiPage)
+	mux.HandleFunc(`/json/`, JSONHandler)
+	mux.HandleFunc(`/`, mainPage)
+	mux.HandleFunc("/update/", updateHandler)
+
+	log.Println("Starting server on: 8080")
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+
+}
