947e714cf07dfc0e375ab4523de3785119e372c3
Ivan Panchenko <arigatory@gmail.com>
2026-01-17 01:17:24 +0300

    add MemStorage

---
+++ internal/repository/memory_storage.go
@@ -0,0 +1,32 @@
+package repository
+
+
+type MemStorage struct {
+	counters map[string]int64
+	gauges   map[string]float64
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		counters: make(map[string]int64),
+		gauges:   make(map[string]float64),
+	}
+}
+
+func (m *MemStorage) UpdateCounter(name string, delta int64) {
+	m.counters[name] += delta
+}
+
+func (m *MemStorage) UpdateGauge(name string, value float64) {
+	m.gauges[name] = value
+}
+
+func (m *MemStorage) GetCounter(name string) (int64, bool) {
+	value, exists := m.counters[name]
+	return value, exists
+}
+
+func (m *MemStorage) GetGauge(name string) (float64, bool) {
+	value, exists := m.gauges[name]
+	return value, exists
+}
