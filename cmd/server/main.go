684c8ab75f1dc7c220dd35fa6219c1fa882336ce
Ivan Panchenko <arigatory@gmail.com>
2026-01-19 01:44:41 +0300

    add flags for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -123,6 +123,8 @@ func (s *server) rootHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	cfg := parseFlags()
+
 	storage := repository.NewMemStorage()
 	srv := &server{storage: storage}
 
@@ -134,8 +136,8 @@ func main() {
 	r.Get("/value/{type}/{name}", srv.valueHandler)
 	r.Get("/", srv.rootHandler)
 
-	log.Println("Starting server on: 8080")
-	err := http.ListenAndServe(`:8080`, r)
+	log.Printf("Starting server on: %s", cfg.Address)
+	err := http.ListenAndServe(cfg.Address, r)
 	if err != nil {
 		log.Fatal(err)
 	}
